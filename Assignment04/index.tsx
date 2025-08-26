f1fe205d11c5aecb2a09879eb06e22d0db9756fb
witchswand <50053539+1106601034@users.noreply.github.com>
2025-08-27 00:22:09 +1000

    Create index.tsx

---
+++ Assignment04/index.tsx
@@ -0,0 +1,30 @@
+console.log('Q1');
+let name = "Alice";
+  if (true) {
+    const name = "Bob";
+    console.log(`The inner name is ${name}`);
+  }
+console.log(`The outer name is ${name}`);
+console.log();
+
+console.log('Q2');
+const DoAddition = (a,b)=>{return a+b;}
+console.log(DoAddition(25,21));
+console.log();
+
+console.log('Q3');
+let greeting = `Hello, ${name}! Welcome to this couese!`;
+console.log(greeting);
+console.log();
+
+console.log('Q4');
+
+console.log();
+
+console.log('Q5');
+
+console.log();
+
+console.log('Q6');
+
+console.log();
