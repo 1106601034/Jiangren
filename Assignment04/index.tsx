11a7d0500d3d8ff9dfc3d14ee648ac1c07d20aa5
witchswand <50053539+1106601034@users.noreply.github.com>
2025-08-27 01:50:25 +1000

    Update index.tsx

--- Assignment04/index.tsx
+++ Assignment04/index.tsx
@@ -1,15 +1,15 @@
 console.log('Q1');
-let name = "Alice";
+let name = "Alice"; // with let type, variable name is able to reassign, also be prevented from unexpected overwrite.
   if (true) {
-    const name = "Bob";
+    const name = "Bob"; // with const type, variable name inside the if block will not effects the outer name.
     console.log(`The inner name is ${name}`);
   }
 console.log(`The outer name is ${name}`);
 console.log();
 
 console.log('Q2');
-const doAddition = (a,b)=>{return a+b;}
-console.log(doAddition(25,21));
+const doAddition = (a,b)=>{return a+b;} // Arrow function inherite context of this keyword from parent scope.
+console.log(doAddition(25,21)); // context of this in normal function is defined by how this function is called.
 console.log();
 
 console.log('Q3');
@@ -19,7 +19,7 @@ console.log();
 
 console.log('Q4');
 let person = {name: "eve", age: 25, city: "Sydney"};
-let {name:personName, age} = person;
+let {name:personName, age} = person; // there is another let type name variable exist in the same scope. rename the newest name variable to avoid error.
 const getParams = ({name,age})=>{return `Name: ${name}, Age: ${age}`;}
 console.log(getParams(person))
 console.log();
@@ -33,9 +33,7 @@ console.log('Q6');
 function sum(a,b) {
     return a + b;
 }
-function doMath (...numbers) {
-    return numbers.reduce(sum,0);
-}
+const doMath = (...numbers) => { return numbers.reduce(sum,0); }
 console.log(doMath(1,2,3,6));
 
 let arr1 = [1,2];
