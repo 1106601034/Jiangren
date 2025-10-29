40f8234d9663cc01f62278a97309636f6ea881f9
1106601034 <christopherchan3003@gmail.com>
2025-10-29 18:02:18 +1000

    update

--- project02/Frontend/app/api/openWeatherApi.ts
+++ project02/Frontend/app/api/openWeatherApi.ts
@@ -1,7 +1,9 @@
-import axios from 'axios';
+import axios from "axios";
 
-export const getForecastData = async() => {
-    const API_KEY = '<YOUR_API_KEY>'
-    const response = await axios.get(`https://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=${API_KEY}&units=metric`);
-    return response.data;
-}
+export const getForecastData = async () => {
+  const API_KEY = "<YOUR_API_KEY>";
+  const response = await axios.get(
+    `https://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=${API_KEY}&units=metric`
+  );
+  return response.data;
+};
