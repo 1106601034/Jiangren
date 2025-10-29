56218998eaa2f74ea79782de644f52544e26a658
1106601034 <christopherchan3003@gmail.com>
2025-10-29 18:27:01 +1000

    a

--- project02/Frontend/app/api/openWeatherApi.ts
+++ project02/Frontend/app/api/openWeatherApi.ts
@@ -2,6 +2,8 @@ import axios from "axios";
 
 export const getForecastData = async () => {
   const API_KEY = "<YOUR_API_KEY>";
+
+  // need to create account to get api key
   const response = await axios.get(
     `https://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=${API_KEY}&units=metric`
   );
